502c486eefdc869a3760a5c7aca5a3420da4eea0
Ram Parthasarathy <rparthasarathy@monetate.com>
2012-04-01 19:41:42 -0400

    Fix function name, spacing and new package

--- safari.safariextension/insertPanel.js
+++ safari.safariextension/insertPanel.js
@@ -1,23 +1,20 @@
 /*
- * Insert the panel when the page action icon is clicked
+ * Insert the panel when the toolbar icon is clicked
  */
 
 if (window.top === window) { // inject only once!
-    safari.self.addEventListener("message", handleGextText, false);
+    safari.self.addEventListener("message", handleIconClick, false);
 }
 
-function handleGextText(event) {
-
+function handleIconClick(event) {
     var messageName = event.name;
     var messageData = event.message;
     if (messageName === "msg") {
         if (messageData === "insert-panel") {
-
-			var script = document.createElement('script');
-			script.type = 'text/javascript';
-			script.src = safari.extension.baseURI + "display.js"
-			document.body.appendChild(script);
-
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = safari.extension.baseURI + "display.js"
+            document.body.appendChild(script);
         }
      }
 }
